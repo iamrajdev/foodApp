977016cc9d93c6e48cdf07ae03febad00cedb577
Raj Dev Barman <iamrajdev1@gmail.com>
2022-09-04 18:23:29 +0530

    Add: Bottom navigation complete

--- app/src/main/java/com/rajdevbarman/foodapp/MainActivity.kt
+++ app/src/main/java/com/rajdevbarman/foodapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.rajdevbarman.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Bottom navigation
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btn_nav);
+        val navController = Navigation.findNavController(this, R.id.host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController);
     }
 }
