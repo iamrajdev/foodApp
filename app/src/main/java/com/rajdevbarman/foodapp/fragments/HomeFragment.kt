81b0cfc879f2a9e9c2504f618f5d183fe549d5c4
Raj Dev Barman <iamrajdev1@gmail.com>
2022-09-05 17:52:29 +0530

    Add: Home fragment debug data complete

--- app/src/main/java/com/rajdevbarman/foodapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/rajdevbarman/foodapp/fragments/HomeFragment.kt
@@ -1,33 +1,26 @@
 package com.rajdevbarman.foodapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.rajdevbarman.foodapp.R
+import com.rajdevbarman.foodapp.pojo.Meal
+import com.rajdevbarman.foodapp.pojo.MealList
+import com.rajdevbarman.foodapp.retrofit.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -38,23 +31,23 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList>{
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                if(response.body() != null){
+                    val randomMeal: Meal = response.body()!!.meals[0]
+                    Log.d("Test", "Meal ID: ${randomMeal.idMeal} - Meal Name: ${randomMeal.strMeal}")
+                } else{
+                    return
                 }
             }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.d("homeFragment", t.message.toString())
+            }
+
+        })
     }
 }
